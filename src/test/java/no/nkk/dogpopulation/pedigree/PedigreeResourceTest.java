59f650a9a36c53343321e8818db9ff47b7c2522f
kimcs <kim.christian.swenson@gmail.com>
2014-02-05 19:43:30 +0100

    Restored a running (although weak) pedigree test.

--- src/test/java/no/nkk/dogpopulation/pedigree/PedigreeResourceTest.java
+++ src/test/java/no/nkk/dogpopulation/pedigree/PedigreeResourceTest.java
@@ -4,7 +4,9 @@ import com.jayway.restassured.RestAssured;
 import no.nkk.dogpopulation.Main;
 import no.nkk.dogpopulation.ResourceConfigFactory;
 import no.nkk.dogpopulation.graph.Dog;
+import no.nkk.dogpopulation.graph.GraphAdminService;
 import no.nkk.dogpopulation.graph.GraphQueryService;
+import no.nkk.dogpopulation.graph.ParentRole;
 import org.apache.commons.io.FileUtils;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -40,6 +42,29 @@ public class PedigreeResourceTest {
         main = new Main(graphDb, resourceConfigFactory, httpPort);
         main.start();
         RestAssured.port = httpPort;
+
+        String breed = "Rottweiler";
+        String childUuid = "uuid-1234567890";
+        String childName = "Wicked teeth Jr. III";
+        String fatherUuid = "uuid-1234567891";
+        String fatherName = "Wicked teeth Sr. II";
+        String fathersFatherUuid = "uuid-1234567892";
+        String fathersFatherName = "Wicked teeth I";
+        String fathersMotherUuid = "uuid-1234567893";
+        String fathersMotherName = "Daisy";
+        String motherUuid = "uuid-1234567894";
+        String motherName = "Tigerclaws";
+
+        GraphAdminService graphAdminService = new GraphAdminService(graphDb);
+        graphAdminService.addDog(childUuid, childName, breed);
+        graphAdminService.addDog(fatherUuid, fatherName, breed);
+        graphAdminService.connectChildToParent(childUuid, fatherUuid, ParentRole.FATHER);
+        graphAdminService.addDog(fathersFatherUuid, fathersFatherName, breed);
+        graphAdminService.connectChildToParent(fatherUuid, fathersFatherUuid, ParentRole.FATHER);
+        graphAdminService.addDog(fathersMotherUuid, fathersMotherName, breed);
+        graphAdminService.connectChildToParent(fatherUuid, fathersMotherUuid, ParentRole.MOTHER);
+        graphAdminService.addDog(motherUuid, motherName, breed);
+        graphAdminService.connectChildToParent(childUuid, motherUuid, ParentRole.MOTHER);
     }
 
     @AfterClass
@@ -51,9 +76,9 @@ public class PedigreeResourceTest {
 
     @Test
     public void thatPedigreeIsWellFormed() throws Exception {
-        Dog dog = RestAssured.expect().statusCode(200).given().parameter("q", "S38454/2006").when().get("/pedigree").as(Dog.class);
+        Dog dog = RestAssured.expect().statusCode(200).given().parameter("uuid", "uuid-1234567891").when().get("/pedigree").as(Dog.class);
         String dogName = dog.getName();
-        Assert.assertEquals(dogName, "A Heaven Sent Habib Ul-Lah");
+        Assert.assertEquals(dogName, "Wicked teeth Sr. II");
     }
 
 }
