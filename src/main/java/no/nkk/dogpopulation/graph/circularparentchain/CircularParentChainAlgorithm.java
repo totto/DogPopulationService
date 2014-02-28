d87a1eb85cfc66a5f6afe50578e78645a6bb7a3e
kimcs <kim.christian.swenson@gmail.com>
2014-02-28 15:05:21 +0100

    Added test and CYPHER algorithm to detect circular parent chains. Not used by application yet.

---
+++ src/main/java/no/nkk/dogpopulation/graph/circularparentchain/CircularParentChainAlgorithm.java
@@ -0,0 +1,45 @@
+package no.nkk.dogpopulation.graph.circularparentchain;
+
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.ResourceIterator;
+import org.neo4j.helpers.collection.IteratorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:kim.christian.swenson@gmail.com">Kim Christian Swenson</a>
+ */
+public class CircularParentChainAlgorithm {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CircularParentChainAlgorithm.class);
+
+    private final GraphDatabaseService graphDb;
+    private final ExecutionEngine engine;
+
+    public CircularParentChainAlgorithm(GraphDatabaseService graphDb) {
+        this.graphDb = graphDb;
+        engine = new ExecutionEngine(graphDb);
+    }
+
+    public List<Relationship> run(String uuid) {
+        StringBuilder query = new StringBuilder();
+        query.append("MATCH (dog:DOG {uuid:{uuid}})-[p:HAS_PARENT*]->(dog)\n");
+        query.append("RETURN p");
+        Map<String, Object> params = new LinkedHashMap<>();
+        params.put("uuid", uuid);
+        ExecutionResult result = engine.execute(query.toString(), params);
+        try (ResourceIterator<Map<String,Object>> iterator = result.iterator()) {
+            for (Map<String,Object> record : IteratorUtil.asIterable(iterator)) {
+                List<Relationship> hasParent = (List<Relationship>) record.get("p");
+                return hasParent;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/no/nkk/dogpopulation/graph/circularparentchain/CircularParentChainAlgorithmTest.java
@@ -0,0 +1,57 @@
+package no.nkk.dogpopulation.graph.circularparentchain;
+
+import no.nkk.dogpopulation.AbstractGraphTest;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:kim.christian.swenson@gmail.com">Kim Christian Swenson</a>
+ */
+public class CircularParentChainAlgorithmTest extends AbstractGraphTest {
+
+    @Test
+    public void thatSimpleCircleCanBeDetected() {
+        Node breedNode = breed("Unit-test Breed", "1");
+        Node A = addDog("A", breedNode);
+        addDog("B", breedNode);
+        addDog("C", breedNode);
+        connectChildToFather("A", "B");
+        connectChildToFather("B", "C");
+        connectChildToFather("C", "A");
+
+        CircularParentChainAlgorithm algorithm = new CircularParentChainAlgorithm(graphDb);
+
+        List<Relationship> circle = algorithm.run("A");
+        Assert.assertNotNull(circle);
+        Assert.assertEquals(circle.size(), 3);
+        List<Relationship> circle2 = algorithm.run("B");
+        Assert.assertNotNull(circle2);
+        Assert.assertEquals(circle2.size(), 3);
+        List<Relationship> circle3 = algorithm.run("C");
+        Assert.assertNotNull(circle3);
+        Assert.assertEquals(circle3.size(), 3);
+    }
+
+    @Test
+    public void thatCorrectPedigreeWontBedetectedAsHavingCircle() {
+        Node breedNode = breed("Unit-test Breed", "1");
+        Node A = addDog("A", breedNode);
+        addDog("B", breedNode);
+        addDog("C", breedNode);
+        connectChildToFather("A", "B");
+        connectChildToFather("B", "C");
+
+        CircularParentChainAlgorithm algorithm = new CircularParentChainAlgorithm(graphDb);
+
+        List<Relationship> circle = algorithm.run("A");
+        Assert.assertNull(circle);
+        List<Relationship> circle2 = algorithm.run("B");
+        Assert.assertNull(circle2);
+        List<Relationship> circle3 = algorithm.run("C");
+        Assert.assertNull(circle3);
+    }
+}
