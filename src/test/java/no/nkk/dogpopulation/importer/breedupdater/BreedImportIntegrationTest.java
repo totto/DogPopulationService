bf027183c0ca304355eadd2ab13e48f0387a06a1
Totto <totto@totto.org>
2014-04-01 15:40:46 +0200

    dump some info on die

--- src/test/java/no/nkk/dogpopulation/importer/breedupdater/BreedImportIntegrationTest.java
+++ src/test/java/no/nkk/dogpopulation/importer/breedupdater/BreedImportIntegrationTest.java
@@ -79,10 +79,10 @@ public class BreedImportIntegrationTest {
                 long allocatedMemory = runtime.totalMemory();
                 long freeMemory = runtime.freeMemory();
 
-                sb.append("free memory: " + format.format(freeMemory / 1024) + "/n");
-                sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "/n");
-                sb.append("max memory: " + format.format(maxMemory / 1024) + "/n");
-                sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "/n");
+                sb.append("free memory: " + format.format(freeMemory / 1024) + "  ");
+                sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "  ");
+                sb.append("max memory: " + format.format(maxMemory / 1024) + "  ");
+                sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "  ");
 
 
                 fail("Unstable version, out of memory. before shotdown\n"+sb);
@@ -101,10 +101,10 @@ public class BreedImportIntegrationTest {
             long allocatedMemory = runtime.totalMemory();
             long freeMemory = runtime.freeMemory();
 
-            sb.append("free memory: " + format.format(freeMemory / 1024) + "/n");
-            sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "/n");
-            sb.append("max memory: " + format.format(maxMemory / 1024) + "/n");
-            sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "/n");
+            sb.append("free memory: " + format.format(freeMemory / 1024) + "  ");
+            sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "  ");
+            sb.append("max memory: " + format.format(maxMemory / 1024) + "  ");
+            sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "  ");
 
 
             fail("Unstable version, out of memory.\n"+sb);
