a3bc7ada26fe74316253f3af8b8f269a99f71802
kimcs <kim.christian.swenson@gmail.com>
2014-04-03 14:03:27 +0200

    Extend incorrect breed test with different breed synonyms in same breed group test that fails due to too simple algorithm.

--- src/test/java/no/nkk/dogpopulation/graph/dataerror/breed/IncorrectBreedAlgorithmTest.java
+++ src/test/java/no/nkk/dogpopulation/graph/dataerror/breed/IncorrectBreedAlgorithmTest.java
@@ -31,4 +31,23 @@ public class IncorrectBreedAlgorithmTest  extends AbstractGraphTest {
         Assert.assertEquals(igr.getUuid(), "A");
     }
 
+    @Test(groups = "fast")
+    public void thatDobermannChildOfTwoDalmatinerSynonymsIsConsideredIncorrectBreed() {
+        Node dalmatiner1 = addBreed("Dalmatiner", "123");
+        Node dalmatiner2 = addBreed("dalmatiner", "123");
+        Node dobermann = addBreed("Dobermann", "456");
+        addDog("A", dobermann);
+        addDog("B", dalmatiner1);
+        addDog("C", dalmatiner2);
+        connectChildToFather("A", "B");
+        connectChildToMother("A", "C");
+
+        List<String> result = graphQueryService.getAllDogsWithInconsistentBreed(0, 10, "Dobermann");
+        Assert.assertNotNull(result);
+        Assert.assertEquals(result.size(), 1);
+
+        IncorrectBreedRecord igr = graphQueryService.getDogWithInconsistentBreed("A");
+        Assert.assertNotNull(igr);
+        Assert.assertEquals(igr.getUuid(), "A");
+    }
 }
