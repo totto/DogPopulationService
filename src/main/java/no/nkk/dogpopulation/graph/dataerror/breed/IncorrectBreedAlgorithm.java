7ad6773fbd8ad778f0056fc2b06b6c4e69b15c49
kimcs <kim.christian.swenson@gmail.com>
2014-04-03 14:04:50 +0200

    Remove unused code in incorrect-breed algorithm.

--- src/main/java/no/nkk/dogpopulation/graph/dataerror/breed/IncorrectBreedAlgorithm.java
+++ src/main/java/no/nkk/dogpopulation/graph/dataerror/breed/IncorrectBreedAlgorithm.java
@@ -1,15 +1,10 @@
 package no.nkk.dogpopulation.graph.dataerror.breed;
 
-import no.nkk.dogpopulation.graph.DogGraphConstants;
-import no.nkk.dogpopulation.graph.DogGraphRelationshipType;
 import no.nkk.dogpopulation.graph.GraphUtils;
-import no.nkk.dogpopulation.graph.dataerror.gender.ChildRecord;
-import no.nkk.dogpopulation.graph.dataerror.gender.IncorrectGenderRecord;
-import no.nkk.dogpopulation.graph.dataerror.gender.LitterRecord;
-import org.joda.time.LocalDate;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
-import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.helpers.collection.IteratorUtil;
 
 import java.util.ArrayList;
@@ -96,86 +91,4 @@ public class IncorrectBreedAlgorithm {
         return null;
     }
 
-
-    private IncorrectGenderRecord populateRecord(Node dogNode) {
-        /*
-         * Dog properties
-         */
-        IncorrectGenderRecord record = new IncorrectGenderRecord();
-        record.setUuid((String) dogNode.getProperty(DogGraphConstants.DOG_UUID));
-        if (dogNode.hasProperty(DogGraphConstants.DOG_NAME)) {
-            record.setName((String) dogNode.getProperty(DogGraphConstants.DOG_NAME));
-        }
-        if (dogNode.hasProperty(DogGraphConstants.DOG_GENDER)) {
-            record.setGender((String) dogNode.getProperty(DogGraphConstants.DOG_GENDER));
-        }
-        if (dogNode.hasProperty(DogGraphConstants.DOG_BORN_YEAR)) {
-            int bornYear = (Integer) dogNode.getProperty(DogGraphConstants.DOG_BORN_YEAR);
-            int bornMonth = (Integer) dogNode.getProperty(DogGraphConstants.DOG_BORN_MONTH);
-            int bornDay = (Integer) dogNode.getProperty(DogGraphConstants.DOG_BORN_DAY);
-            record.setBorn(new LocalDate(bornYear, bornMonth, bornDay).toString());
-        }
-        Relationship isBreed = dogNode.getSingleRelationship(DogGraphRelationshipType.IS_BREED, Direction.OUTGOING);
-        Node breedNode = isBreed.getEndNode();
-        record.setBreed((String) breedNode.getProperty(DogGraphConstants.BREEDSYNONYM_SYNONYM));
-
-        /*
-         * Litters
-         */
-        List<LitterRecord> litters = new ArrayList<>();
-        for (Relationship hasLitter : dogNode.getRelationships(Direction.OUTGOING, DogGraphRelationshipType.HAS_LITTER)) {
-            Node litterNode = hasLitter.getEndNode();
-            LitterRecord litterRecord = new LitterRecord();
-            if (litterNode.hasProperty(DogGraphConstants.LITTER_ID)) {
-                litterRecord.setId((String) litterNode.getProperty(DogGraphConstants.LITTER_ID));
-            }
-            if (litterNode.hasProperty(DogGraphConstants.LITTER_COUNT)) {
-                litterRecord.setCount((Integer) litterNode.getProperty(DogGraphConstants.LITTER_COUNT));
-            }
-            if (litterNode.hasProperty(DogGraphConstants.LITTER_YEAR)) {
-                int bornYear = (Integer) litterNode.getProperty(DogGraphConstants.LITTER_YEAR);
-                int bornMonth = (Integer) litterNode.getProperty(DogGraphConstants.LITTER_MONTH);
-                int bornDay = (Integer) litterNode.getProperty(DogGraphConstants.LITTER_DAY);
-                litterRecord.setBorn(new LocalDate(bornYear, bornMonth, bornDay).toString());
-            }
-            if (hasLitter.hasProperty(DogGraphConstants.HASLITTER_ROLE)) {
-                litterRecord.setRole((String) hasLitter.getProperty(DogGraphConstants.HASLITTER_ROLE));
-            }
-            litters.add(litterRecord);
-        }
-        record.setLitters(litters);
-
-        /*
-         * Children
-         */
-        List<ChildRecord> children = new ArrayList<>();
-        for (Relationship hasParent : dogNode.getRelationships(Direction.INCOMING, DogGraphRelationshipType.HAS_PARENT)) {
-            Node childNode = hasParent.getStartNode();
-            ChildRecord childRecord = new ChildRecord();
-            childRecord.setUuid((String) childNode.getProperty(DogGraphConstants.DOG_UUID));
-            if (childNode.hasProperty(DogGraphConstants.DOG_BORN_YEAR)) {
-                int bornYear = (Integer) childNode.getProperty(DogGraphConstants.DOG_BORN_YEAR);
-                int bornMonth = (Integer) childNode.getProperty(DogGraphConstants.DOG_BORN_MONTH);
-                int bornDay = (Integer) childNode.getProperty(DogGraphConstants.DOG_BORN_DAY);
-                childRecord.setBorn(new LocalDate(bornYear, bornMonth, bornDay).toString());
-            }
-            if (hasParent.hasProperty(DogGraphConstants.HASLITTER_ROLE)) {
-                childRecord.setParentRole((String) hasParent.getProperty(DogGraphConstants.HASLITTER_ROLE));
-            }
-            for (Relationship otherHasParent : childNode.getRelationships(Direction.OUTGOING, DogGraphRelationshipType.HAS_PARENT)) {
-                if (otherHasParent.equals(hasParent)) {
-                    continue;
-                }
-                Node otherParentNode = otherHasParent.getEndNode();
-                childRecord.setOtherParentUuid((String) otherParentNode.getProperty(DogGraphConstants.DOG_UUID));
-                childRecord.setOtherParentName((String) otherParentNode.getProperty(DogGraphConstants.DOG_NAME));
-                childRecord.setOtherParentRole((String) otherHasParent.getProperty(DogGraphConstants.HASPARENT_ROLE));
-                break; // only consider first other hasParent relationship
-            }
-            children.add(childRecord);
-        }
-        record.setChildren(children);
-
-        return record;
-    }
 }
