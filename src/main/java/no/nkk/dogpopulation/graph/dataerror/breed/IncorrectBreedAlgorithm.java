dd757a9032b73a7a0d71ed8b4c1f6534a36e66a3
kimcs <kim.christian.swenson@gmail.com>
2014-04-03 14:07:34 +0200

    Improved incorrect breed algorithm to consider breed, not just breed synonyms in comparison.

--- src/main/java/no/nkk/dogpopulation/graph/dataerror/breed/IncorrectBreedAlgorithm.java
+++ src/main/java/no/nkk/dogpopulation/graph/dataerror/breed/IncorrectBreedAlgorithm.java
@@ -39,20 +39,22 @@ public class IncorrectBreedAlgorithm {
         query.append("MATCH \n");
         query.append("  (d:DOG)-[:HAS_PARENT {role:'father'}]->(f), \n");
         query.append("  (d)-[:HAS_PARENT {role:'mother'}]->(m), \n");
-        query.append("  (d)-[:IS_BREED]->(bd), \n");
-        query.append("  (f)-[:IS_BREED]->(bf), \n");
-        query.append("  (m)-[:IS_BREED]->(bm), \n");
-        query.append("  (bd)-[:MEMBER_OF]->(theBreed:BREED) \n");
+        query.append("  (d)-[:IS_BREED]->(bsd)-[:MEMBER_OF]->(bd), \n");
+        query.append("  (f)-[:IS_BREED]->(bsf)-[:MEMBER_OF]->(bf), \n");
+        query.append("  (m)-[:IS_BREED]->(bsm) \n");
         query.append("WHERE \n");
-        query.append("  bf = bm AND bd <> bf \n");
-        query.append("  AND ID(theBreed)={breedNodeId} \n");
+        query.append("  bd <> bf \n");
+        query.append("  AND ID(bd)={breedNodeId} \n");
+        query.append("WITH d, bsm, bf\n");
+        query.append("MATCH \n");
+        query.append("  (bsm)-[:MEMBER_OF]->(bf)");
         query.append("RETURN \n");
         query.append("  d.uuid \n");
         query.append("ORDER BY d.uuid \n");
         query.append("SKIP ").append(skip).append(" \n");
         query.append("LIMIT ").append(limit).append(" \n");
-        ExecutionResult result1 = engine.execute(query.toString(), params);
-        try (ResourceIterator<Map<String,Object>> iterator = result1.iterator()) {
+        ExecutionResult result = engine.execute(query.toString(), params);
+        try (ResourceIterator<Map<String,Object>> iterator = result.iterator()) {
             for (Map<String,Object> record : IteratorUtil.asIterable(iterator)) {
                 list.add((String) record.get("d.uuid"));
             }
@@ -67,24 +69,22 @@ public class IncorrectBreedAlgorithm {
         query.append("MATCH \n");
         query.append("  (d:DOG {uuid:{uuid}})-[:HAS_PARENT {role:'father'}]->(f), \n");
         query.append("  (d)-[:HAS_PARENT {role:'mother'}]->(m), \n");
-        query.append("  (d)-[:IS_BREED]->(bd), \n");
-        query.append("  (f)-[:IS_BREED]->(bf), \n");
-        query.append("  (m)-[:IS_BREED]->(bm) \n");
-        query.append("WHERE \n");
-        query.append("  bf = bm AND bd <> bf \n");
+        query.append("  (d)-[:IS_BREED]->(bsd), \n");
+        query.append("  (f)-[:IS_BREED]->(bsf), \n");
+        query.append("  (m)-[:IS_BREED]->(bsm) \n");
         query.append("RETURN \n");
-        query.append("  d.uuid, f.uuid, m.uuid, bd.synonym, bf.synonym, bm.synonym \n");
+        query.append("  d.uuid, f.uuid, m.uuid, bsd.synonym, bsf.synonym, bsm.synonym \n");
         query.append("ORDER BY d.uuid \n");
-        ExecutionResult result1 = engine.execute(query.toString(), params);
-        try (ResourceIterator<Map<String,Object>> iterator = result1.iterator()) {
+        ExecutionResult result = engine.execute(query.toString(), params);
+        try (ResourceIterator<Map<String,Object>> iterator = result.iterator()) {
             for (Map<String,Object> record : IteratorUtil.asIterable(iterator)) {
                 IncorrectBreedRecord ibr = new IncorrectBreedRecord();
                 ibr.setUuid((String) record.get("d.uuid"));
-                ibr.setBreed((String) record.get("bd.synonym"));
+                ibr.setBreed((String) record.get("bsd.synonym"));
                 ibr.setFatherUuid((String) record.get("f.uuid"));
-                ibr.setFatherBreed((String) record.get("bf.synonym"));
+                ibr.setFatherBreed((String) record.get("bsf.synonym"));
                 ibr.setMotherUuid((String) record.get("m.uuid"));
-                ibr.setMotherBreed((String) record.get("bm.synonym"));
+                ibr.setMotherBreed((String) record.get("bsm.synonym"));
                 return ibr;
             }
         }
